876ef0e9e1aabd6c21df83ff7c83505638b2c670
Marla Schulz <marlaa.schulz@gmail.com>
2021-09-26 16:58:38 -0700

    Update temperatures to farenheit

--- spec/poros/forecast_spec.rb
+++ spec/poros/forecast_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe Forecast do
         dt: 1632692756,
         sunrise: 1632660694,
         sunset: 1632703840,
-        temp: 303.99,
-        feels_like: 301.95,
+        temp: 83.88,
+        feels_like: 81.01,
         pressure: 1008,
         humidity: 12,
         visibility: 10000,
@@ -46,8 +46,8 @@ RSpec.describe Forecast do
           sunrise: 1632660694,
           sunset: 1632703840,
             temp: {
-                min: 291.12,
-                max: 303.99,
+                min: 57.31,
+                max: 77.9,
             },
             weather: [
                 {
@@ -61,8 +61,8 @@ RSpec.describe Forecast do
           sunrise: 1632660694,
           sunset: 1632703840,
           temp: {
-              min: 292.17,
-              max: 303.84,
+              min: 55.71,
+              max: 74.05,
           },
           weather: [
               {
@@ -76,8 +76,8 @@ RSpec.describe Forecast do
           sunrise: 1632660694,
           sunset: 1632703840,
           temp: {
-              min: 291.12,
-              max: 303.99,
+            min: 57.31,
+            max: 77.9,
           },
           weather: [
               {
@@ -91,8 +91,8 @@ RSpec.describe Forecast do
           sunrise: 1632660694,
           sunset: 1632703840,
           temp: {
-              min: 292.17,
-              max: 303.84,
+            min: 55.71,
+            max: 74.05,
           },
           weather: [
               {
@@ -106,8 +106,8 @@ RSpec.describe Forecast do
           sunrise: 1632660694,
           sunset: 1632703840,
           temp: {
-              min: 292.17,
-              max: 303.84,
+            min: 55.71,
+            max: 74.05,
           },
           weather: [
               {
@@ -126,8 +126,8 @@ RSpec.describe Forecast do
       datetime: '2021-09-26 14:45:56 -0700',
       sunrise: '2021-09-26 05:51:34 -0700',
       sunset: '2021-09-26 17:50:40 -0700',
-      temperature: 303.99,
-      feels_like: 301.95,
+      temperature: 83.88,
+      feels_like: 81.01,
       humidity: 12.0,
       visibility: 10000.0,
       conditions: 'broken clouds',
@@ -142,8 +142,8 @@ RSpec.describe Forecast do
       datetime: '2021-09-26 14:45:56 -0700',
       sunrise: '2021-09-26 05:51:34 -0700',
       sunset: '2021-09-26 17:50:40 -0700',
-      min_temp: 291.12,
-      max_temp: 303.99,
+      min_temp: 57.31,
+      max_temp: 77.9,
       conditions: "few clouds",
       icon: "02d",
     },
@@ -151,8 +151,8 @@ RSpec.describe Forecast do
       datetime: '2021-09-26 14:45:56 -0700',
       sunrise: '2021-09-26 05:51:34 -0700',
       sunset: '2021-09-26 17:50:40 -0700',
-      min_temp: 292.17,
-      max_temp: 303.84,
+      min_temp: 55.71,
+      max_temp: 74.05,
       conditions: 'broken clouds',
       icon: '04d',
     },
@@ -160,8 +160,8 @@ RSpec.describe Forecast do
       datetime: '2021-09-26 14:45:56 -0700',
       sunrise: '2021-09-26 05:51:34 -0700',
       sunset: '2021-09-26 17:50:40 -0700',
-      min_temp: 291.12,
-      max_temp: 303.99,
+      min_temp: 57.31,
+      max_temp: 77.9,
       conditions: "few clouds",
       icon: "02d",
     },
@@ -169,8 +169,8 @@ RSpec.describe Forecast do
       datetime: '2021-09-26 14:45:56 -0700',
       sunrise: '2021-09-26 05:51:34 -0700',
       sunset: '2021-09-26 17:50:40 -0700',
-      min_temp: 292.17,
-      max_temp: 303.84,
+      min_temp: 55.71,
+      max_temp: 74.05,
       conditions: 'broken clouds',
       icon: '04d',
     },
@@ -178,8 +178,8 @@ RSpec.describe Forecast do
       datetime: '2021-09-26 14:45:56 -0700',
       sunrise: '2021-09-26 05:51:34 -0700',
       sunset: '2021-09-26 17:50:40 -0700',
-      min_temp: 292.17,
-      max_temp: 303.84,
+      min_temp: 55.71,
+      max_temp: 74.05,
       conditions: 'broken clouds',
       icon: '04d',
     }]
