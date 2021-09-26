4726bc90757d6c7b51c2788b159e7df002673222
Marla Schulz <marlaa.schulz@gmail.com>
2021-09-26 16:53:22 -0700

    Create forecast poros

---
+++ app/poros/forecast.rb
@@ -0,0 +1,40 @@
+class Forecast
+  attr_reader :current_weather,
+              :daily_weather,
+              :hourly_weather
+
+  def initialize(data)
+    @current_weather = current(data[:current])
+    @daily_weather = daily(data[:daily])
+    #@hourly_weather = hourly(data[:hourly])
+    #@id = nil
+  end
+
+  def current(data)
+    {
+      datetime: Time.at(data[:dt]),
+      sunrise: Time.at(data[:sunrise]),
+      sunset: Time.at(data[:sunset]),
+      temperature: data[:temp],
+      feels_like: data[:feels_like],
+      humidity: data[:humidity].to_f,
+      visibility: data[:visibility].to_f,
+      conditions: data[:weather].first[:description],
+      icon: data[:weather].first[:icon]
+    }
+  end
+
+  def daily(data)
+    data[0..4].map do |data|
+      {
+        datetime: Time.at(data[:dt]).to_s,
+        sunrise: Time.at(data[:sunrise]).to_s,
+        sunset: Time.at(data[:sunset]).to_s,
+        max_temp: data[:temp][:max],
+        min_temp: data[:temp][:min],
+        conditions: data[:weather].first[:description],
+        icon: data[:weather].first[:icon],
+      }
+    end
+  end
+end
---
+++ spec/poros/forecast_spec.rb
@@ -0,0 +1,189 @@
+require 'rails_helper'
+
+RSpec.describe Forecast do
+  before(:each) do
+    @data = { current: {
+        dt: 1632692756,
+        sunrise: 1632660694,
+        sunset: 1632703840,
+        temp: 303.99,
+        feels_like: 301.95,
+        pressure: 1008,
+        humidity: 12,
+        visibility: 10000,
+        weather: [
+            {
+                description: "broken clouds",
+                icon: "04d"
+            }
+        ]
+    },
+    hourly: [
+        {
+            dt: 1632690000,
+            temp: 303.98,
+            weather: [
+                {
+                    description: "broken clouds",
+                    icon: "04d"
+                }
+            ]
+        },
+        {
+            dt: 1632693600,
+            temp: 303.99,
+            weather: [
+                {
+                    description: "broken clouds",
+                    icon: "04d"
+                }
+            ]
+        }
+    ],
+    daily: [
+        {
+          dt: 1632692756,
+          sunrise: 1632660694,
+          sunset: 1632703840,
+            temp: {
+                min: 291.12,
+                max: 303.99,
+            },
+            weather: [
+                {
+                    description: "few clouds",
+                    icon: "02d"
+                }
+            ]
+        },
+        {
+          dt: 1632692756,
+          sunrise: 1632660694,
+          sunset: 1632703840,
+          temp: {
+              min: 292.17,
+              max: 303.84,
+          },
+          weather: [
+              {
+                description: "broken clouds",
+                icon: "04d"
+              }
+          ]
+        },
+        {
+          dt: 1632692756,
+          sunrise: 1632660694,
+          sunset: 1632703840,
+          temp: {
+              min: 291.12,
+              max: 303.99,
+          },
+          weather: [
+              {
+                description: "few clouds",
+                icon: "02d"
+              }
+            ]
+        },
+        {
+          dt: 1632692756,
+          sunrise: 1632660694,
+          sunset: 1632703840,
+          temp: {
+              min: 292.17,
+              max: 303.84,
+          },
+          weather: [
+              {
+                description: "broken clouds",
+                icon: "04d"
+              }
+            ]
+        },
+        {
+          dt: 1632692756,
+          sunrise: 1632660694,
+          sunset: 1632703840,
+          temp: {
+              min: 292.17,
+              max: 303.84,
+          },
+          weather: [
+              {
+                description: "broken clouds",
+                icon: "04d"
+              }
+            ]
+        }
+    ]
+  }
+    @forecast = Forecast.new(@data)
+  end
+
+  it 'can hash current weather' do
+    expected = {
+      datetime: '2021-09-26 14:45:56 -0700',
+      sunrise: '2021-09-26 05:51:34 -0700',
+      sunset: '2021-09-26 17:50:40 -0700',
+      temperature: 303.99,
+      feels_like: 301.95,
+      humidity: 12.0,
+      visibility: 10000.0,
+      conditions: 'broken clouds',
+      icon: '04d',
+    }
+
+    expect(@forecast.current(@data[:current])).to eq(expected)
+  end
+
+  it 'can create array of 5 days of weather' do
+    expected = [{
+      datetime: '2021-09-26 14:45:56 -0700',
+      sunrise: '2021-09-26 05:51:34 -0700',
+      sunset: '2021-09-26 17:50:40 -0700',
+      min_temp: 291.12,
+      max_temp: 303.99,
+      conditions: "few clouds",
+      icon: "02d",
+    },
+    {
+      datetime: '2021-09-26 14:45:56 -0700',
+      sunrise: '2021-09-26 05:51:34 -0700',
+      sunset: '2021-09-26 17:50:40 -0700',
+      min_temp: 292.17,
+      max_temp: 303.84,
+      conditions: 'broken clouds',
+      icon: '04d',
+    },
+    {
+      datetime: '2021-09-26 14:45:56 -0700',
+      sunrise: '2021-09-26 05:51:34 -0700',
+      sunset: '2021-09-26 17:50:40 -0700',
+      min_temp: 291.12,
+      max_temp: 303.99,
+      conditions: "few clouds",
+      icon: "02d",
+    },
+    {
+      datetime: '2021-09-26 14:45:56 -0700',
+      sunrise: '2021-09-26 05:51:34 -0700',
+      sunset: '2021-09-26 17:50:40 -0700',
+      min_temp: 292.17,
+      max_temp: 303.84,
+      conditions: 'broken clouds',
+      icon: '04d',
+    },
+    {
+      datetime: '2021-09-26 14:45:56 -0700',
+      sunrise: '2021-09-26 05:51:34 -0700',
+      sunset: '2021-09-26 17:50:40 -0700',
+      min_temp: 292.17,
+      max_temp: 303.84,
+      conditions: 'broken clouds',
+      icon: '04d',
+    }]
+    expect(@forecast.daily(@data[:daily]).length).to eq(5)
+    expect(@forecast.daily(@data[:daily])).to eq(expected)
+  end
+end
