f71ce7093cee84349777c05991dc70917d3bbf7c
Marla Schulz <marlaa.schulz@gmail.com>
2021-09-27 15:46:38 -0700

    Add testing

--- spec/requests/api/v1/new_user_request_spec.rb
+++ spec/requests/api/v1/new_user_request_spec.rb
@@ -3,26 +3,38 @@ require 'rails_helper'
 RSpec.describe 'create user' do
   describe 'create' do
     it 'can create a new user' do
-      merchant = create(:merchant)
-      old_count = Item.all.count
+      create_list(:user, 4)
+      old_count = User.all.count
 
-      post '/api/v1/items', params: {
-        name: 'Bouncy Ball',
-        description: 'It bounces',
-        unit_price: 15.40,
-        merchant_id: merchant.id
+      post '/api/v1/users', params: {
+        email: 'sparky@dog.net',
+        password: 'playtime',
+        password_confirmation: 'playtime'
       }, as: :json
       expect(response).to be_successful
 
-      item = JSON.parse(response.body, symbolize_names: true)
+      user = JSON.parse(response.body, symbolize_names: true)
 
-      new_count = Item.all.count
-      expect(item[:data][:type]).to eq('item')
-      expect(item[:data][:attributes][:name]).to eq('Bouncy Ball')
-      expect(item[:data][:attributes][:description]).to eq('It bounces')
-      expect(item[:data][:attributes][:unit_price]).to eq(15.40)
-      expect(item[:data][:attributes][:merchant_id]).to eq(merchant.id)
+      new_count = User.all.count
+      expect(user[:data]).to have_key(:id)
+
+      expect(user[:data][:type]).to eq('users')
+      expect(user[:data][:attributes][:email]).to eq('sparky@dog.net')
+      expect(user[:data][:attributes]).to have_key(:api_key)
+      expect(user[:data][:attributes][:api_key]).to be_a(String)
       expect(new_count).to eq(old_count + 1)
+    end
+
+    it "sends error if passwords don't match" do
+
+    end
+
+    it 'sends error if email already taken' do
+
+    end
+
+    it 'sends error if fields are missing' do
+      
     end
   end
 end
