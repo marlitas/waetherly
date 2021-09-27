dd70d2a60b94ed2793261bfa4b466439c0ab9804
Marla Schulz <marlaa.schulz@gmail.com>
2021-09-27 16:04:52 -0700

    Add relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :api_keys, as: :bearer
   validates :email, presence: true, uniqueness: true
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User do
+  it {should have_many(:api_keys)}
   it {should validate_presence_of(:password)}
   it {should validate_uniqueness_of(:email)}
 end
