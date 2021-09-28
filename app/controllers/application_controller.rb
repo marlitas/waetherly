c4f67833f544805dc71c1d0d05f524265aa4401b
Marla Schulz <marlaa.schulz@gmail.com>
2021-09-28 10:49:52 -0700

    Add current user for authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::API
-  protect_from_forgery with: :exception
+  def current_user
+    User.find(session[:user_id]) if !session[:user_id].nil?
+  end
 end
